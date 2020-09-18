a977129f41c71ede0836bd36bb94760426880db5
Saketh Ram <sakethram48@gmail.com>
2020-09-18 23:38:06 +0530

    Done with dynamicjs

---
+++ script.js
@@ -0,0 +1,39 @@
+let sArray = [];
+function pushData(form, event) {
+  event.preventDefault();
+  var sObject = {name: form.name.value, email: form.email.value, section: form.section.value, number: form.number.value,
+  };
+  sArray.push(sObject);
+  showData();
+  clearForm(form);
+}
+
+function showData() {
+  let targetBody = document.querySelector("tbody");
+  targetBody.innerHTML = '';
+  sArray.forEach((student) => {
+    targetBody.innerHTML += `
+             <tr>
+                <td>
+                    ${student.name}
+                </td>
+                <td>
+                    ${student.email}
+                </td>
+                <td>
+                    ${student.section}
+                </td>
+                <td>
+                    ${student.number}
+                </td>
+            </tr>
+      `;
+  });
+}
+
+function clearForm(form) {
+  form.name.value = "";
+  form.email.value = "";
+  form.section.value = "";
+  form.number.value = "";
+}
